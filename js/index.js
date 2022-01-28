2a53263b1a68fe292ee992c045f6a246e7e6592e
daisuke245 <daisuke5220@gmail.com>
2022-01-28 18:18:50 -0500

    Listado dinámico de artículos/posts

--- js/index.js
+++ js/index.js
@@ -1,8 +1,22 @@
 $(document).ready(function(){
+    const jsonPosts = JSON.parse(posts);
+    const divPosts = $('#posts');
+    
     $('.bxslider').bxSlider({
         mode: 'fade',
         captions: true,
         slideWidth: 1200, 
         auto: true
+    });
+    
+    jsonPosts.forEach((item, index) => {
+        item.date = moment().format('MMMM dddd YYYY');
+        let post = `<article class="post">
+                        <h2>${item.title}</h2>
+                        <span class="date">${item.date}</span>
+                        <p>${item.content}</p>
+                        <a href="#" class="button-more">Leer mas</a>
+                    </article>`
+        divPosts.append(post);
     });
 });
