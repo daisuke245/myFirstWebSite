b1067e98b6af7a6e692a2b3838b894651c1f80bf
daisuke245 <daisuke5220@gmail.com>
2022-02-02 19:24:01 -0500

    Cambiador de temas y scroll automatico

--- js/index.js
+++ js/index.js
@@ -1,12 +1,26 @@
 $(document).ready(function(){
     const jsonPosts = JSON.parse(posts);
     const divPosts = $('#posts');
+    const theme = $('#theme');
     
     $('.bxslider').bxSlider({
         mode: 'fade',
         captions: true,
         slideWidth: 1200, 
         auto: true
+    });
+    
+    $('.color-theme').click( (e)=>{
+        switch (e.target.id){
+            case 'to-green':
+                theme.attr('href','css/green.css');
+                break;
+            case 'to-blue':
+                theme.attr('href','css/blue.css');
+                break;
+            case 'to-red':
+                theme.attr('href','css/red.css');
+        }
     });
     
     jsonPosts.forEach((item, index) => {
@@ -18,5 +32,15 @@ $(document).ready(function(){
                         <a href="#" class="button-more">Leer mas</a>
                     </article>`
         divPosts.append(post);
+    });
+    
+    $('.subir').click(function(e){
+        e.preventDefault();
+        
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+        
+        return false;
     });
 });
